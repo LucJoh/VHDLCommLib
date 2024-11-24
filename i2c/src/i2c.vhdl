33d29dea87ebff462d22ea533f654337f5b2d898
lucjoh <lucjoh99@hotmail.com>
2024-11-24 20:26:40 +0100

    address state working

--- i2c/src/i2c.vhdl
+++ i2c/src/i2c.vhdl
@@ -6,7 +6,7 @@
 -- Author     : lucjoh
 -- Company    : 
 -- Created    : 2024-08-23
--- Last update: 2024-09-22
+-- Last update: 2024-11-24
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -22,283 +22,171 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.i2c_pkg.all;
 
 entity i2c is
+  generic (
+    sys_clk_period : integer := 10;     -- The period of the system clock (ns)
+    scl_freq       : integer := 100;    -- The frequency of SCL (kHz)
+    addrwidth      : integer := 7;      -- number of bits in the address bus
+    datawidth      : integer := 8;      -- number of bits in the data bus
+    databytes      : integer := 1  -- number of data bytes following the address byte
+    );
   port (
-    clk     : in    std_logic;
-    rstn    : in    std_logic;
-    i2c_in  : in    i2c_in_type;
-    i2c_out : out   i2c_out_type;
-    scl     : inout std_logic;
-    sda     : inout std_logic
-   --ack        : buffer std_logic
+    clk  : in std_ulogic;
+    rstn : in std_ulogic;
+
+    rw    : in std_ulogic;
+    start : in std_ulogic;
+
+    addr    : in  std_ulogic_vector(addrwidth-1 downto 0);
+    tx_data : in  std_ulogic_vector(datawidth-1 downto 0);
+    rx_data : out std_ulogic_vector(datawidth-1 downto 0);
+
+    ready : out std_ulogic;
+    done  : out std_ulogic;
+
+    scl : out   std_logic;
+    sda : inout std_logic
     );
 end i2c;
 
 architecture rtl of i2c is
 
-  type state_type is (idle, addr, data);
+  constant sys_clk_counts : integer := 1000000 / (sys_clk_period * scl_freq);
 
-  type reg_type is record
-    state            : state_type;
-    i                : integer;         -- data index
-    byte_counter     : integer;         -- bytes to send/receive
-    tx_addr          : std_logic_vector(addrwidth downto 0);  -- address to send + rw bit
-    tx_data          : std_logic_vector(datawidth - 1 downto 0);  -- data to send
-    rx_data          : std_logic_vector(datawidth - 1 downto 0);  -- received data
-    rx_data_temp     : std_logic_vector(datawidth - 1 downto 0);  -- received data
-    rw               : std_logic;       -- read/write bit
-    dcl              : std_logic;       -- data clock (shifted scl)
-    dcl_enable       : std_logic;
-    dcl_prev         : std_logic;
-    dcl_falling_edge : boolean;
-    dcl_rising_edge  : boolean;
-    dcl_sample       : boolean;
-    dcl_counter      : integer;
-    scl              : std_logic;
-    scl_enable       : std_logic;
-    scl_prev         : std_logic;
-    scl_falling_edge : boolean;
-    scl_rising_edge  : boolean;
-    scl_sample       : boolean;
-    scl_counter      : integer;
-    sda              : std_logic;
-    sda_enable       : std_logic;
-    ack              : std_logic;
-    ack_prev         : std_logic;
-    ack_falling_edge : boolean;
-    ack_rising_edge  : boolean;
-    done             : std_logic;       -- SPI transfer done
-    ready            : std_logic;       -- SPI master ready for transfer
-  end record;
+  type state_type is (idle_state, addr_state, data_state);
 
-  -- default register values
-  constant reg_init : reg_type := (state            => idle,
-                                   i                => 0,
-                                   byte_counter     => 0,
-                                   tx_addr          => (others => '0'),
-                                   tx_data          => (others => '0'),
-                                   rx_data          => (others => '0'),
-                                   rx_data_temp     => (others => '0'),
-                                   rw               => '0',
-                                   dcl              => '1',
-                                   dcl_enable       => '0',
-                                   dcl_prev         => '1',
-                                   dcl_falling_edge => false,
-                                   dcl_rising_edge  => false,
-                                   dcl_sample       => false,
-                                   dcl_counter      => 0,
-                                   scl              => 'Z',
-                                   scl_enable       => '0',
-                                   scl_prev         => '1',
-                                   scl_falling_edge => false,
-                                   scl_rising_edge  => false,
-                                   scl_sample       => false,
-                                   scl_counter      => 0,
-                                   sda              => 'Z',
-                                   sda_enable       => '0',
-                                   ack              => '0',
-                                   ack_prev         => '1',
-                                   ack_falling_edge => false,
-                                   ack_rising_edge  => false,
-                                   done             => '0',
-                                   ready            => '0');
+  signal state        : state_type := idle_state;
+  signal i            : integer    := 0;
+  signal byte_counter : integer    := 0;
 
-  signal r, rin : reg_type := reg_init;
+  signal tx_addr      : std_ulogic_vector(addrwidth downto 0)     := (others => '0');  -- address + rw
+  signal rx_data_temp : std_ulogic_vector(datawidth - 1 downto 0) := (others => '0');
 
----------- begin architecture ------------
+  signal scl_local        : std_ulogic := 'Z';
+  signal scl_enable       : std_ulogic := '0';
+  signal scl_local_prev   : std_ulogic := '1';
+  signal scl_falling_edge : boolean    := false;
+  signal scl_rising_edge  : boolean    := false;
+  signal scl_counter      : integer    := 0;
+
+  signal sda_local  : std_ulogic := 'Z';
+  signal sda_enable : std_ulogic := '0';
+
+  signal start_prev         : std_ulogic := '1';
+  signal start_falling_edge : boolean    := false;
+  signal start_rising_edge  : boolean    := false;
+
+-------------------------------------------------------  
+-- BEGIN ARCHITECTURE 
+-------------------------------------------------------
 
 begin
 
-  combinational : process(i2c_in, sda, r) is
-    variable v : reg_type;
+  clock_gen_process : process(clk) is
   begin
-
-    ----------- default assignment -----------
-
-    v    := r;
-    v.rw := i2c_in.rw;
-    --v.ack  := ack;
-
-    ---------------- algorithm ---------------
-
-    -- DCL generation
-    if r.dcl_enable = '1' then
-      v.scl_enable := '1';
-      if (r.dcl_counter = sys_clk_counts) then
-        v.dcl_counter := 0;
-        v.dcl         := not v.dcl;
-      else
-        v.dcl_counter := v.dcl_counter + 1;
-      end if;
-    else
-      v.scl_enable := '0';
-      v.dcl        := '1';
-    end if;
-
-    -- SCL generation
-    if r.scl_enable = '1' then
-      if (r.scl_counter = sys_clk_counts) then
-        v.scl_counter := 0;
-        v.scl         := not v.scl;
-      else
-        v.scl_counter := v.scl_counter + 1;
-      end if;
-    else
-      v.scl := '1';
-    end if;
-
-    -- DCL edge detection
-    v.dcl_rising_edge  := v.dcl = '1' and v.dcl_prev = '0';
-    v.dcl_falling_edge := v.dcl = '0' and v.dcl_prev = '1';
-    v.dcl_prev         := v.dcl;
-
-    -- SCL edge detection
-    v.scl_rising_edge  := v.scl = '1' and v.scl_prev = '0';
-    v.scl_falling_edge := v.scl = '0' and v.scl_prev = '1';
-    v.scl_prev         := v.scl;
-
-    -- ACK edge detection
-    --v.ack_rising_edge  := v.ack = '1' and v.ack_prev = '0';
-    --v.ack_falling_edge := v.ack = '0' and v.ack_prev = '1';
-    --v.ack_prev         := v.ack;
-
-    -- I2C state machine 
-    case r.state is
-
-      when idle =>
-
-        v.done := '0';
-        if i2c_in.start = '1' then
-          v.done       := '0';
-          v.i          := addrwidth;
-          v.ready      := '0';
-          v.state      := addr;
-          v.tx_addr    := i2c_in.addr & i2c_in.rw;
-          v.rw         := i2c_in.rw;
-          v.tx_data    := i2c_in.tx_data;
-          v.sda        := '0';
-          v.sda_enable := '1';
-          v.dcl        := '0';
+    if rising_edge(clk) then
+      -------------------------------------------------  
+      -- SCL GENERATION
+      -------------------------------------------------  
+      if scl_enable = '1' then
+        if (scl_counter = sys_clk_counts) then
+          scl_counter <= 0;
+          scl_local   <= not scl_local;
         else
-          v.ready            := '1';
-          v.done             := '0';
-          v.sda              := 'Z';
-          v.sda_enable       := '0';
-          v.dcl              := '1';
-          v.dcl_enable       := '0';
-          v.dcl_prev         := '1';
-          v.dcl_rising_edge  := false;
-          v.dcl_falling_edge := false;
-          v.dcl_counter      := 0;
-          v.scl              := 'Z';
-          v.scl_enable       := '0';
-          v.scl_prev         := '1';
-          v.scl_rising_edge  := false;
-          v.scl_falling_edge := false;
-          v.scl_counter      := 0;
-          v.scl_enable       := '0';
+          scl_counter <= scl_counter + 1;
         end if;
-
-      when addr =>
-
-        v.dcl_enable := '1';
-        if r.dcl_rising_edge then
-          --v.sda := v.tx_addr(v.i);
-          --v.i   := v.i - 1;
-          if r.i = 0 then
-            v.sda_enable := '0';
-          end if;
-          if r.i = -1 then
-            -- ack
-            if r.sda = '0' then
-              v.state      := data;
-              v.sda        := 'Z';
-              v.sda_enable := not v.rw;  -- only enable sda if writing
-              v.i          := datawidth - 1;
-            -- nack  
-            else
-              v.state := idle;
-            end if;
-          else
-            v.sda := v.tx_addr(v.i);
-            v.i   := v.i - 1;
-          end if;
-        end if;
-
-      when data =>
-
-        if r.dcl_rising_edge then
-          -- write  
-          if r.rw = '0' then
-            v.sda := v.tx_data(v.i);
-            v.i   := v.i - 1;
-            if r.i = 0 then
-              v.sda_enable := '0';
-            end if;
-            if r.i = -1 then
-              -- ack
-              if r.sda = '0' then
-                if r.byte_counter = databytes then
-                  v.state := idle;
-                  v.scl   := 'Z';
-                  v.done  := '1';       -- done flag set because of ack
-                else
-                  v.byte_counter := v.byte_counter + 1;
-                  v.i            := datawidth - 1;
-                end if;
-              -- nack
-              else
-                v.state := idle;        -- done flag not set because of nack
-              end if;
-            end if;
-          -- read 
-          else
-            v.rx_data_temp(v.i) := v.sda;
-            v.i                 := v.i - 1;
-            if r.i = -1 then
-              v.rx_data := v.rx_data_temp;
-            elsif r.i = -2 then         -- skip ack when reading
-              if r.byte_counter = databytes then
-                v.state      := idle;
-                v.scl_enable := '0';
-                v.done       := '1';
-              else
-                v.byte_counter := v.byte_counter + 1;
-                v.i            := datawidth - 1;
-              end if;
-            end if;
-          end if;
-        end if;
-
-      when others =>
-
-        -- nothing
-
-    end case;
-
-    ----- register input to seq process -----
-
-    rin <= v;
-
-    ------------- entity output -------------
-
-    i2c_out.done    <= r.done;
-    i2c_out.ready   <= r.ready;
-    i2c_out.rx_data <= r.rx_data;
-    sda             <= r.sda when r.sda_enable = '1' else 'Z';
-    scl             <= r.scl when r.scl_enable = '1' else 'Z';
-
-  end process;
-
-  sequential : process(clk) is
-  begin
-    if rstn = '0' then
-      r <= reg_init;
-    elsif rising_edge(clk) then
-      r <= rin;
+      else
+        scl_local <= '1';
+      end if;
     end if;
   end process;
+
+  edge_detect_process : process(clk) is
+  begin
+    if rising_edge(clk) then
+      -------------------------------------------------  
+      -- SCL DETECTION
+      -------------------------------------------------  
+      scl_rising_edge    <= scl_local = '1' and scl_local_prev = '0';
+      scl_falling_edge   <= scl_local = '0' and scl_local_prev = '1';
+      scl_local_prev     <= scl_local;
+      -------------------------------------------------  
+      -- START DETECTION
+      -------------------------------------------------  
+      start_rising_edge  <= start = '1' and start_prev = '0';
+      start_falling_edge <= start = '0' and start_prev = '1';
+      start_prev         <= start;
+    end if;
+  end process;
+
+  fsm_process : process(addr, clk, rw, start_rising_edge, state) is
+  begin
+    if rising_edge(clk) then
+      ---------------------------------------------------
+      -- I2C STATE MACHINE
+      ---------------------------------------------------
+      case state is
+
+        when idle_state =>
+
+          done <= '0';
+          if start_rising_edge then
+            done       <= '0';
+            i          <= addrwidth;
+            ready      <= '0';
+            state      <= addr_state;
+            tx_addr    <= addr & rw;
+            sda_enable <= '1';
+            sda_local  <= '0';
+          else
+            ready      <= '1';
+            done       <= '0';
+            sda_local  <= '0';
+            sda_enable <= '0';
+            scl_enable <= '0';
+          end if;
+
+        when addr_state =>
+
+          scl_enable <= '1';
+          if scl_falling_edge then
+            if i > -1 then
+              sda_local <= tx_addr(i);
+            end if;
+            i <= i - 1;
+          end if;
+          if i = -1 then
+            sda_enable <= '0';
+          end if;
+
+          if scl_rising_edge and i = -2 then
+            --------------------------------------------
+            -- ACK
+            --------------------------------------------
+            if sda = '0' then
+              state <= data_state;
+              i     <= datawidth;
+            --------------------------------------------
+            --NACK
+            --------------------------------------------
+            else
+              state <= idle_state;
+            end if;
+          end if;
+
+        when data_state =>
+
+
+        when others =>
+
+          -- nothing
+
+      end case;
+    end if;
+  end process;
+
+  scl <= scl_local when scl_enable = '1' else '1';
+  sda <= sda_local when sda_enable = '1' else 'Z';
 
 end architecture;
--- i2c/testbench/tb.vhdl
+++ i2c/testbench/tb.vhdl
@@ -4,84 +4,91 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use std.textio.all;
-use work.i2c_pkg.all;
 
 entity tb is
-  generic (runner_cfg : string);
+  generic (
+    runner_cfg     : string;
+    sys_clk_period : integer := 10;     -- The period of the system clock (ns)
+    scl_freq       : integer := 100;    -- The frequency of SCL (kHz)
+    addrwidth      : integer := 7;      -- number of bits in the address bus
+    datawidth      : integer := 8;      -- number of bits in the data bus
+    databytes      : integer := 1  -- number of data bytes following the address byte
+    );
 end entity;
 
 architecture rtl of tb is
 
-  signal clk         : std_logic                                := '0';
-  signal dcl_tb      : std_logic                                := '1';
-  signal rstn        : std_logic                                := '0';
-  signal i2c_in      : i2c_in_type;
-  signal i2c_out     : i2c_out_type;
-  signal sda         : std_logic                                := 'Z';
-  signal scl         : std_logic                                := 'Z';
-  signal tx_data     : std_logic_vector(datawidth - 1 downto 0) := "11110001";
-  signal addr        : std_logic_vector(addrwidth - 1 downto 0) := "1110111";
-  signal clk_counter : integer                                  := 0;
+  signal clk         : std_ulogic                                := '0';
+  signal rstn        : std_ulogic                                := '0';
+  signal start       : std_ulogic                                := '0';
+  signal rw          : std_ulogic                                := '0';
+  signal ready       : std_ulogic                                := '0';
+  signal done        : std_ulogic                                := '0';
+  signal sda         : std_logic                                 := 'Z';
+  signal scl         : std_ulogic                                := 'Z';
+  signal addr        : std_ulogic_vector(addrwidth - 1 downto 0) := "1110111";
+  signal tx_data     : std_ulogic_vector(datawidth - 1 downto 0) := "11110001";
+  signal rx_data     : std_ulogic_vector(datawidth-1 downto 0)   := (others => '0');
+  signal clk_counter : integer                                   := 0;
 
   constant clk_period : time := 10 ns;
 
 begin
 
-  -- instantiate the SPI master component
+  -------------------------------------------------------------
+  -- DUT
+  -------------------------------------------------------------
   i2c_inst : entity work.i2c
     port map (
-      clk     => clk,
-      rstn    => rstn,
-      i2c_in  => i2c_in,
-      i2c_out => i2c_out,
-      scl     => scl,
-      sda     => sda
+      clk     => clk,                   -- i 
+      rstn    => rstn,                  -- i 
+      start   => start,                 -- i
+      rw      => rw,                    -- i
+      addr    => addr,                  -- i
+      tx_data => tx_data,               -- i
+      ready   => ready,                 -- i
+      rx_data => rx_data,               -- o
+      done    => done,                  -- o
+      scl     => scl,                   -- io
+      sda     => sda                    -- io 
       );
 
-  -- system clock generation
+  --------------------------------------------------------------
+  -- SYSTEM CLOCK GENERATION
+  --------------------------------------------------------------
   clk <= not clk after clk_period / 2;
 
-  -- tb dcl generation process
-  dcl_gen : process (clk)
-  begin
-    if rising_edge(clk) then
-      if i2c_out.ready = '0' then
-        if clk_counter = sys_clk_counts then
-          dcl_tb      <= not dcl_tb;
-          clk_counter <= 0;
-        else
-          clk_counter <= clk_counter + 1;
-        end if;
-      else
-        dcl_tb <= '1';
-      end if;
-    end if;
-  end process;
-
-  -- stimulus process
+  --------------------------------------------------------------
+  -- STIMULUS PROCESS
+  --------------------------------------------------------------
   stimulus_process : process
   begin
 
     test_runner_setup(runner, runner_cfg);
 
-    -- reset the system
-    rstn <= '0';
-    wait for 20 ns;
-    rstn <= '1';
-    wait for 20 ns;
+    sda <= 'Z';
 
-    -- write operation test (master sends data)
-    i2c_in.start   <= '1';
-    i2c_in.rw      <= '0';              -- write operation
-    i2c_in.addr    <= addr;             -- address to write to
-    i2c_in.tx_data <= tx_data;          -- data to be sent
+    ------------------------------------------------------------
+    -- RESET THE SYSTEM
+    ------------------------------------------------------------
+   -- rstn <= '0';
+   -- wait for 20 ns;
+   -- rstn <= '1';
+    wait for 10000 ns;
+
+    ------------------------------------------------------------
+    -- WRITE OPERATION TEST (MASTER SENDS DATA)
+    ------------------------------------------------------------
+    start   <= '1';
+    rw      <= '0';              -- write operation
 
     wait for clk_period;
 
-    i2c_in.start <= '0';
-    --dcl_tb       <= '0';
+    start <= '0';
 
-    -- wait for address bits to be transmitted
+    -------------------------------------------------------------
+    -- WAIT FOR ADDRESS BITS TO BE TRANSMITTED
+    -------------------------------------------------------------
     for i in addrwidth downto 0 loop
       wait until rising_edge(scl);
     end loop;
@@ -91,46 +98,20 @@ begin
     wait until rising_edge(scl);
     sda <= 'Z';                         -- release SDA line
 
-    -- wait for done signal to indicate end of transmission
-    if i2c_out.done /= '1' then
-      wait until i2c_out.done = '1';
+    wait for 10000 ns;
+
+    report "-------- TEST FINISHED SUCESSFULLY --------";
+    test_runner_cleanup(runner);
+    wait;
+
+    --------------------------------------------------------------
+    -- WAIT FOR DONE SIGNAL TO INDICATE END OF TRANSMISSION
+    --------------------------------------------------------------
+    if done /= '1' then
+      wait until done = '1';
     end if;
     wait for 1000 ns;
 
---      -- read operation test (master reads data)
---      spi_in.enable  <= '1';
---      spi_in.rw      <= '1';              -- read operation
---      spi_in.tx_addr <= (others => '0');  -- example address, adjust as needed
---      spi_in.tx_data <= (others => 'X');  -- don't care data
---
---      -- wait for the correct number of SCLK edges before starting MISO
---      for i in 0 to pre_miso_bits - 1 loop
---        wait until falling_edge(spi_out.sclk);
---        spi_in.enable <= '0';
---      end loop;
---
---      wait until falling_edge(spi_out.sclk);
---
---      -- feed the MISO line with the data bits
---      for i in datawidth - 1 downto 0 loop
---        spi_in.miso <= miso(i);
---        wait until falling_edge(spi_out.sclk);
---      end loop;
---
---      -- reset MISO line after transmission
---      spi_in.miso <= 'X';
---
---      -- wait for done signal to indicate end of transmission
---      wait until spi_out.done = '1';
---      wait for 100 ns;
-
-    -- stop the simulation
---    report "-------- WRITE OPERATTION --------";
---    report "TO BE SENT : ADDRESS : " & to_string(tx_addr) & " DATA : " & to_string(tx_data);
---    report "DETECTED ON MOSI LINE : ADDRESS : " & to_string(mosi(addrwidth + datawidth - 1 downto datawidth)) & " DATA : " & to_string(mosi(datawidth - 1 downto 0));
---    report "-------- READ OPERATTION --------";
---    report "DATA SENT ON MISO LINE : " & to_string(miso);
---    report "DATA DETECTED BY MASTER : " & to_string(spi_out.rx_data);
     report "-------- TEST FINISHED SUCESSFULLY --------";
     test_runner_cleanup(runner);
     wait;
