0ed01c1bcbdcd49e15585266715c70d5bc772f9e
Lucas Johansson <lucas@Lucass-Mac-mini.local>
2024-12-20 14:42:32 -0800

    vunit version 5

--- i2c/run.py
+++ i2c/run.py
@@ -1,7 +1,7 @@
 from vunit import VUnit
 from os.path import join, dirname
 
-VU = VUnit.from_argv(compile_builtins=False)
+VU = VUnit.from_argv()
 VU.add_vhdl_builtins()  # Add the VHDL builtins explicitly!
 
 ROOT = dirname(__file__)
@@ -12,7 +12,7 @@ i2c.add_source_files(join(ROOT, "testbench", "*.vhdl"))
 
 # Add waveform automatically when running in GUI mode.
 i2c.set_sim_option("modelsim.init_file.gui","waves/wave.do")
-i2c.set_sim_option("ghdl.gtkwave_script.gui","waves/wave.tcl")
-i2c.set_sim_option("nvc.gtkwave_script.gui","waves/wave.tcl")
+i2c.set_sim_option("ghdl.viewer_script.gui","waves/wave.tcl")
+i2c.set_sim_option("nvc.viewer_script.gui","waves/wave.tcl")
 
 VU.main()
